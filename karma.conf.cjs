a2aa5e3a96522bd70701a2e4ee34881d501e10db
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-02-03 22:47:41 -0300

    configures karma-coverage

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -34,13 +34,14 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
+            'src/**/*.js': ['karma-coverage']
         },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
 
 
         // web server port
@@ -71,6 +72,11 @@ module.exports = function (config) {
 
         // Concurrency level
         // how many browser should be started simultaneous
-        concurrency: Infinity
+        concurrency: Infinity,
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/'
+        }
     })
 }
