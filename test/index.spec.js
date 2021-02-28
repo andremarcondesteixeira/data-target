d7536c715a67c57f2619d8576a13773d2ff215ef
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-02-27 21:16:37 -0300

    refactors index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,19 +1,29 @@
 import { getAugmentedAnchors } from '../src/index.js';
 
 describe('foo', () => {
-    it('select all anchors with data-augmented attribute', () => {
-        const anchors = [true, true, false, true].map(targetIsSelector => createAnchor(targetIsSelector));
+    it('select all anchors with data-module attribute', () => {
+        const anchors = [
+            createAnchor(),
+            createAnchorWithDataTarget(),
+            createAnchorWithDataTarget(),
+            createAnchorWithDataTarget()
+        ];
         const container = createDivWith(anchors);
         const selected = getAugmentedAnchors(container);
         assert.equal(selected.length, 3);
     });
 });
 
-function createAnchor(targetIsSelector) {
+function createAnchor() {
     const anchor = document.createElement('a');
     anchor.href = '#';
     anchor.target = '_self';
-    targetIsSelector && anchor.setAttribute('data-augmented', null);
+    return anchor;
+}
+
+function createAnchorWithDataTarget() {
+    const anchor = createAnchor();
+    anchor.setAttribute('data-target', '#my-element-id');
     return anchor;
 }
 
