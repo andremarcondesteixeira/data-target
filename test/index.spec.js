2901ac8f3ce03fd2120b21606e3042df4d3eb54f
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-02-27 23:04:47 -0300

    refactors a test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,23 @@
 import { getAugmentedAnchorsFrom } from '../src/index.js';
 
-describe('getAugmentedAnchorsFrom', () => {
-    it('select all anchors with data-target and/or data-module attribute', () => {
+describe('function getAugmentedAnchorsFrom', () => {
+    function createAnchor(extraAttributes = {}) {
+        const anchor = document.createElement('a');
+        anchor.href = '#';
+        anchor.target = '_self';
+        for (let attribute in extraAttributes) {
+            anchor.setAttribute(attribute, extraAttributes[attribute]);
+        }
+        return anchor;
+    }
+
+    function createDivWith(anchors) {
+        const div = document.createElement('div');
+        anchors.forEach(anchor => div.appendChild(anchor));
+        return div;
+    }
+
+    it('selects all anchors with data-target and/or data-module attribute', () => {
         const anchors = [
             createAnchor(),
             createAnchor({ 'data-target': '#my-element-id' }),
@@ -11,21 +27,5 @@ describe('getAugmentedAnchorsFrom', () => {
         const container = createDivWith(anchors);
         const selected = getAugmentedAnchorsFrom(container);
         assert.equal(selected.length, 3);
-
-        function createAnchor(extraAttributes = {}) {
-            const anchor = document.createElement('a');
-            anchor.href = '#';
-            anchor.target = '_self';
-            for (let attribute in extraAttributes) {
-                anchor.setAttribute(attribute, extraAttributes[attribute]);
-            }
-            return anchor;
-        }
-
-        function createDivWith(anchors) {
-            const div = document.createElement('div');
-            anchors.forEach(anchor => div.appendChild(anchor));
-            return div;
-        }
     });
 });
