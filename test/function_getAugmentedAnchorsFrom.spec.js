3da43d53bbae3497daf676beb748cdf9f5d3cb42
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-02-28 20:01:17 -0300

    (RED) divides a test in two

--- test/function_getAugmentedAnchorsFrom.spec.js
+++ test/function_getAugmentedAnchorsFrom.spec.js
@@ -1,18 +1,9 @@
 import { getAugmentedAnchorsFrom } from '../src/index.js';
 
 describe('function getAugmentedAnchorsFrom', () => {
-    it('selects all anchors with data-target and/or data-module attribute and ignores anchors with invalid data attributes combinations', () => {
+    it('selects all anchors with valid data-target and data-module attributes combinations', () => {
         const rootElement = document.createElement('div');
         rootElement.innerHTML = `
-            <a id="IGNORED-non-augmented"                                   href="#"                                              ></a>
-            <a id="IGNORED-with-empty-data-target-without-data-module-A"    href="#" data-target=""                               ></a>
-            <a id="IGNORED-with-empty-data-target-without-data-module-B"    href="#" data-target                                  ></a>
-            <a id="IGNORED-without-data-target-with-empty-data-module-A"    href="#"                        data-module=""        ></a>
-            <a id="IGNORED-without-data-target-with-empty-data-module-B"    href="#"                        data-module           ></a>
-            <a id="IGNORED-with-empty-data-target-with-empty-data-module-A" href="#" data-target=""         data-module=""        ></a>
-            <a id="IGNORED-with-empty-data-target-with-empty-data-module-B" href="#" data-target            data-module=""        ></a>
-            <a id="IGNORED-with-empty-data-target-with-empty-data-module-C" href="#" data-target=""         data-module           ></a>
-            <a id="IGNORED-with-empty-data-target-with-empty-data-module-D" href="#" data-target            data-module           ></a>
             <a id="OK-with-data-target-without-data-module"                 href="#" data-target="whatever"                       ></a>
             <a id="OK-with-data-target-with-empty-data-module-A"            href="#" data-target="whatever" data-module=""        ></a>
             <a id="OK-with-data-target-with-empty-data-module-B"            href="#" data-target="whatever" data-module           ></a>
@@ -23,5 +14,22 @@ describe('function getAugmentedAnchorsFrom', () => {
         `;
         const selected = getAugmentedAnchorsFrom(rootElement);
         assert.equal(selected.length, 7);
+    });
+
+    it('ignores all anchors with invalid data-target and data-module attributes combinations', () => {
+        const rootElement = document.createElement('div');
+        rootElement.innerHTML = `
+            <a id="IGNORED-non-augmented"                                   href="#"                                      ></a>
+            <a id="IGNORED-with-empty-data-target-without-data-module-A"    href="#" data-target=""                       ></a>
+            <a id="IGNORED-with-empty-data-target-without-data-module-B"    href="#" data-target                          ></a>
+            <a id="IGNORED-without-data-target-with-empty-data-module-A"    href="#"                        data-module=""></a>
+            <a id="IGNORED-without-data-target-with-empty-data-module-B"    href="#"                        data-module   ></a>
+            <a id="IGNORED-with-empty-data-target-with-empty-data-module-A" href="#" data-target=""         data-module=""></a>
+            <a id="IGNORED-with-empty-data-target-with-empty-data-module-B" href="#" data-target            data-module=""></a>
+            <a id="IGNORED-with-empty-data-target-with-empty-data-module-C" href="#" data-target=""         data-module   ></a>
+            <a id="IGNORED-with-empty-data-target-with-empty-data-module-D" href="#" data-target            data-module   ></a>
+        `;
+        const selected = getAugmentedAnchorsFrom(rootElement);
+        assert.equal(selected.length, 0);
     });
 });
