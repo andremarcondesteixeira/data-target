409b32cf69184a110a22a94b0e9b3b3acaaddff9
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-03-08 20:37:45 -0300

    (RED) changes rules: an anchor with a data-module but without a data-target is now ignored

--- test/function_getAugmentedAnchorsFrom.spec.js
+++ test/function_getAugmentedAnchorsFrom.spec.js
@@ -6,18 +6,12 @@ describe('function getAugmentedAnchorsFrom', () => {
             <a id="OK-with-data-target-without-data-module"      href="#" data-target="whatever"                       ></a>
             <a id="OK-with-data-target-with-empty-data-module-A" href="#" data-target="whatever" data-module=""        ></a>
             <a id="OK-with-data-target-with-empty-data-module-B" href="#" data-target="whatever" data-module           ></a>
-            <a id="OK-without-data-target-with-data-module"      href="#"                        data-module="whatever"></a>
-            <a id="OK-with-empty-data-target-with-data-module-A" href="#" data-target=""         data-module="whatever"></a>
-            <a id="OK-with-empty-data-target-with-data-module-B" href="#" data-target            data-module="whatever"></a>
             <a id="OK-with-data-target-with-data-module"         href="#" data-target="whatever" data-module="whatever"></a>
         `;
         const expectedIDs = [
             'OK-with-data-target-without-data-module',
             'OK-with-data-target-with-empty-data-module-A',
             'OK-with-data-target-with-empty-data-module-B',
-            'OK-without-data-target-with-data-module',
-            'OK-with-empty-data-target-with-data-module-A',
-            'OK-with-empty-data-target-with-data-module-B',
             'OK-with-data-target-with-data-module'
         ].sort();
 
@@ -26,22 +20,25 @@ describe('function getAugmentedAnchorsFrom', () => {
         const selectedAnchors = getAugmentedAnchorsFrom(rootElement);
         const selectedIDs = Array.from(selectedAnchors).map(element => element.id).sort();
 
-        expect(selectedAnchors.length).to.equal(7);
+        expect(selectedAnchors.length).to.equal(4);
         expect(expectedIDs.toString()).to.equal(selectedIDs.toString());
     });
 
     it('should ignore all anchors with invalid data-target and data-module attributes combinations', () => {
         const rootElement = document.createElement('div');
         rootElement.innerHTML = `
-            <a id="IGNORED-non-augmented"                                   href="#"                              ></a>
-            <a id="IGNORED-with-empty-data-target-without-data-module-A"    href="#" data-target=""               ></a>
-            <a id="IGNORED-with-empty-data-target-without-data-module-B"    href="#" data-target                  ></a>
-            <a id="IGNORED-without-data-target-with-empty-data-module-A"    href="#"                data-module=""></a>
-            <a id="IGNORED-without-data-target-with-empty-data-module-B"    href="#"                data-module   ></a>
-            <a id="IGNORED-with-empty-data-target-with-empty-data-module-A" href="#" data-target="" data-module=""></a>
-            <a id="IGNORED-with-empty-data-target-with-empty-data-module-B" href="#" data-target    data-module=""></a>
-            <a id="IGNORED-with-empty-data-target-with-empty-data-module-C" href="#" data-target="" data-module   ></a>
-            <a id="IGNORED-with-empty-data-target-with-empty-data-module-D" href="#" data-target    data-module   ></a>
+            <a id="IGNORED-non-augmented"                                   href="#"                                      ></a>
+            <a id="IGNORED-with-empty-data-target-without-data-module-A"    href="#" data-target=""                       ></a>
+            <a id="IGNORED-with-empty-data-target-without-data-module-B"    href="#" data-target                          ></a>
+            <a id="IGNORED-without-data-target-with-empty-data-module-A"    href="#"                data-module=""        ></a>
+            <a id="IGNORED-without-data-target-with-empty-data-module-B"    href="#"                data-module           ></a>
+            <a id="IGNORED-with-empty-data-target-with-empty-data-module-A" href="#" data-target="" data-module=""        ></a>
+            <a id="IGNORED-with-empty-data-target-with-empty-data-module-B" href="#" data-target    data-module=""        ></a>
+            <a id="IGNORED-with-empty-data-target-with-empty-data-module-C" href="#" data-target="" data-module           ></a>
+            <a id="IGNORED-with-empty-data-target-with-empty-data-module-D" href="#" data-target    data-module           ></a>
+            <a id="IGNORED-without-data-target-with-data-module"            href="#"                data-module="whatever"></a>
+            <a id="IGNORED-with-empty-data-target-with-data-module-A"       href="#" data-target="" data-module="whatever"></a>
+            <a id="IGNORED-with-empty-data-target-with-data-module-B"       href="#" data-target    data-module="whatever"></a>
         `;
         const selectedAnchors = getAugmentedAnchorsFrom(rootElement);
         assert.equal(selectedAnchors.length, 0);
