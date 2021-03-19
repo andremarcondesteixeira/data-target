e140d4bf3e60e2b805cb0cb993e61cd9ce914662
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-03-19 12:04:15 -0300

    (RED) removing support for data-module attribute: removes test for data-module attribute and a test module used in this test

--- test/function_overrideAnchorsBehavior.spec.js
+++ test/function_overrideAnchorsBehavior.spec.js
@@ -1,5 +1,4 @@
 import { overrideAnchorsBehavior } from '../src/augmented-anchors.js';
-import { setListenerForTestModule, getCount } from './contents/test-module.js';
 
 describe('function overrideAnchorsBehavior', () => {
     it('should render content inside the first element found by a valid data-target selector', () => {
@@ -16,25 +15,6 @@ describe('function overrideAnchorsBehavior', () => {
         return doTest(rootElement, getTrigger(rootElement), finish => {
             const targetTestContent = rootElement.querySelector('#test-element').querySelector('.test-content');
             expect(targetTestContent).to.not.be.null;
-            finish();
-        });
-    });
-
-    it('should load a javascript module after content is rendered when both data-target and data-module attributes are valid', () => {
-        const rootElement = document.createElement('div');
-        rootElement.innerHTML = `
-            <a href="/base/test/contents/test-content.html"
-               data-target="#test-element"
-               data-module="/base/test/contents/test-module.js"></a>
-            <div id="test-element">
-                <!-- CONTENT SHOULD BE RENDERED HERE -->
-            </div>`;
-
-        setListenerForTestModule(rootElement);
-        overrideAnchorsBehavior(rootElement);
-
-        return doTest(rootElement, getTrigger(rootElement), finish => {
-            expect(getCount()).to.be.equal(1);
             finish();
         });
     });
