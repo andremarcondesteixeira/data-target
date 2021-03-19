109f38f9d520ab6bee6a2b741850ea79fa01fe24
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-03-19 18:08:04 -0300

    (REFACTOR) refactors a test

--- test/function_enableAnchorSelectorTarget.spec.js
+++ test/function_enableAnchorSelectorTarget.spec.js
@@ -5,17 +5,22 @@ describe('function enableAnchorsTargetSelectors', () => {
         const rootElement = document.createElement('div');
         rootElement.innerHTML = `
             <a href="/base/test/contents/test-content.html"
-               data-target-selector="#test-element"></a>
-            <div id="test-element">
-                <!-- CONTENT SHOULD BE RENDERED HERE -->
-            </div>`;
+               data-target-selector="#main .content">anchor</a>
+            <main id="main">
+                <section class="content" id="target">
+                    <!-- CONTENT SHOULD BE RENDERED HERE -->
+                </section>
+                <section class="content" id="not-the-target">
+                    <!-- CONTENT SHOULD NOT BE RENDERED HERE -->
+                </section>
+            </main>`;
 
-            enableAnchorsTargetSelectors(rootElement);
+        enableAnchorsTargetSelectors(rootElement);
 
         return new Promise(resolve => {
-            rootElement.addEventListener('hats:DOMContentLoaded', function onContentLoaded() {
-                const targetTestContent = rootElement.querySelector('#test-element').querySelector('.test-content');
-                expect(targetTestContent).to.not.be.null;
+            rootElement.addEventListener('hats:DOMContentLoaded', function onContentLoaded(event) {
+                const renderedElement = rootElement.querySelector('#target').querySelector('.test-content');
+                expect(renderedElement).to.not.be.null;
                 rootElement.removeEventListener('hats:DOMContentLoaded', onContentLoaded);
                 resolve();
             });
