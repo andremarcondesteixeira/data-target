e80f7a91da235ea2995ea0b6a7bd2dfdb9b0893b
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-03-19 18:19:00 -0300

    (RED) adding url to the event details: defines an assertion for it in a test

--- test/function_enableAnchorSelectorTarget.spec.js
+++ test/function_enableAnchorSelectorTarget.spec.js
@@ -1,11 +1,10 @@
 import { enableAnchorsTargetSelectors } from '../src/hats.js';
 
 describe('function enableAnchorsTargetSelectors', () => {
-    it('should render content inside the first element found by a valid data-target-selector attribute', () => {
+    it('should render content inside the first element found by a valid data-target-selector attribute and dispatches a hats:DOMContentLoaded event', () => {
         const rootElement = document.createElement('div');
         rootElement.innerHTML = `
-            <a href="/base/test/contents/test-content.html"
-               data-target-selector="#main .content">anchor</a>
+            <a href="/base/test/contents/test-content.html" data-target-selector="#main .content">anchor</a>
             <main id="main">
                 <!-- CONTENT SHOULD BE RENDERED INSIDE THE ELEMENT BELOW -->
                 <section class="content" id="target"></section>
@@ -22,6 +21,8 @@ describe('function enableAnchorsTargetSelectors', () => {
 
                 const notTheTarget = rootElement.querySelector('#not-the-target');
                 expect(notTheTarget.textContent).to.be.equal('this content must not change');
+
+                expect(event.details).to.be.equal('http://localhost:9876/base/test/contents/test-content.html');
 
                 rootElement.removeEventListener('hats:DOMContentLoaded', onContentLoaded);
                 resolve();
