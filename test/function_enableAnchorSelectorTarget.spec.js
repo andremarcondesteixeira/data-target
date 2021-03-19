fb5a41a901a147831889eed9bd5479372a618cc5
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-03-19 20:43:02 -0300

    (REFACTOR) refactors tests

--- test/function_enableAnchorSelectorTarget.spec.js
+++ test/function_enableAnchorSelectorTarget.spec.js
@@ -2,8 +2,7 @@ import { enableAnchorsTargetSelectors } from '../src/hats.js';
 
 describe('function enableAnchorsTargetSelectors', () => {
     it('should render content inside the first element found by a valid data-target-selector attribute and dispatches a hats:DOMContentLoaded event', () => {
-        const rootElement = document.createElement('div');
-        rootElement.innerHTML = `
+        const html = `
             <a href="/base/test/contents/test-content.html" data-target-selector="#main .content">anchor</a>
             <main id="main">
                 <!-- CONTENT SHOULD BE RENDERED INSIDE THE ELEMENT BELOW -->
@@ -12,46 +11,41 @@ describe('function enableAnchorsTargetSelectors', () => {
                 <section class="content" id="not-the-target">this content must not change</section>
             </main>`;
 
-        enableAnchorsTargetSelectors(rootElement);
-
-        return new Promise(resolve => {
-            rootElement.addEventListener('hats:DOMContentLoaded', event => {
-                const renderedElement = rootElement.querySelector('#target').querySelector('.test-content');
-                expect(renderedElement).to.not.be.null;
-
-                const notTheTarget = rootElement.querySelector('#not-the-target');
-                expect(notTheTarget.textContent).to.be.equal('this content must not change');
-
-                expect(event.detail.href).to.be.equal('http://localhost:9876/base/test/contents/test-content.html');
-
-                resolve();
-            });
-
-            rootElement.querySelector('a').click();
+        return doTest(html, (finish, root, event) => {
+            expect(root.querySelector('#target').querySelector('.test-content')).to.not.be.null;
+            expect(root.querySelector('#not-the-target').textContent).to.be.equal('this content must not change');
+            expect(event.detail.href).to.be.equal('http://localhost:9876/base/test/contents/test-content.html');
+            finish();
         });
     });
 
     it('should enable data-target-selector in nested anchors', () => {
-        const rootElement = document.createElement('div');
-        rootElement.innerHTML = `
+        const html = `
             <a href="/base/test/contents/nested.html" data-target-selector="#content">anchor</a>
             <section id="content"></section>`;
 
-        enableAnchorsTargetSelectors(rootElement);
-
-        return new Promise(resolve => {
-            rootElement.addEventListener('hats:DOMContentLoaded', () => {
-                const renderedElement = rootElement.querySelector('#inner-content');
-                renderedElement.addEventListener('hats:DOMContentLoaded', () => {
-                    const content = renderedElement.querySelector('.test-content');
-                    expect(content).to.not.be.null;
-                    resolve();
-                });
-
-                rootElement.querySelector('#content a').click();
+        return doTest(html, (finish, root) => {
+            root.querySelector('#inner-content').addEventListener('hats:DOMContentLoaded', () => {
+                expect(root.querySelector('.test-content')).to.not.be.null;
+                finish();
             });
 
-            rootElement.querySelector('a').click();
+            root.querySelector('#content a').click();
         });
     });
 });
+
+function doTest(html, testFunction) {
+    const rootElement = document.createElement('div');
+    rootElement.innerHTML = html;
+
+    enableAnchorsTargetSelectors(rootElement);
+
+    return new Promise(resolve => {
+        rootElement.addEventListener('hats:DOMContentLoaded', event => {
+            testFunction(() => resolve(), rootElement, event);
+        });
+
+        rootElement.querySelector('a').click();
+    });
+}
