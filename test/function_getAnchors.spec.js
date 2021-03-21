c2080c62776fab221246e4ec9815ca5b630ddb60
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-03-21 20:07:31 -0300

    (RED) changing the lib to look for id's instead

--- test/function_getAnchors.spec.js
+++ test/function_getAnchors.spec.js
@@ -3,8 +3,8 @@ import { getAnchors } from '../src/hati.js';
 describe('function getAnchors', () => {
     it('should select all anchors with a non-empty data-target-id attribute', () => {
         const html = `
-            <a id="anchor1" href="#" data-target-id="#someId">anchor 1</a>
-            <a id="anchor2" href="#" data-target-id=".someClass">anchor 1</a>
+            <a id="anchor1" href="#" data-target-id="someId">anchor 1</a>
+            <a id="anchor2" href="#" data-target-id="someOtherId">anchor 2</a>
         `;
         const expectedIDs = [
             'anchor1',
@@ -24,8 +24,8 @@ describe('function getAnchors', () => {
         const rootElement = document.createElement('div');
         rootElement.innerHTML = `
             <a id="no-data-target-id"      href="#"               ></a>
-            <a id="empty-data-target-id-1" href="#" data-target=""></a>
-            <a id="empty-data-target-id-2" href="#" data-target   ></a>
+            <a id="empty-data-target-id-1" href="#" data-target-id=""></a>
+            <a id="empty-data-target-id-2" href="#" data-target-id   ></a>
         `;
         const selectedAnchors = getAnchors(rootElement);
         assert.equal(selectedAnchors.length, 0);
