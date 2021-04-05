b80a064bc7ce360c72538555607550b13c51761f
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-04-05 00:11:53 -0300

    (REFACTOR) turns most functions into nested functions to lower the number of overall parameters

--- src/hati.js
+++ src/hati.js
@@ -1,93 +1,88 @@
-export function initialize(rootElement, options) {
-    const anchors = getAnchors(rootElement);
-    anchors.forEach(anchor => {
-        anchor.addEventListener('click', event => {
-            dispatchBeforeLoadEvent(anchor);
-            history.pushState({}, null, anchor.href);
-            tryLoadContent(event, anchor, rootElement, options);
+export function initialize(config) {
+    const anchors = getAnchors(config.root);
+    anchors.forEach(a => a.addEventListener('click', handleClick));
+
+    function handleClick(event) {
+        dispatchBeforeLoadEvent(event.target);
+        history.pushState({}, null, event.target.href);
+        tryLoadContent(event);
+    }
+
+    function dispatchBeforeLoadEvent(anchor) {
+        const event = new CustomEvent('hati:beforeLoad', {
+            bubbles: true,
+            cancelable: true,
+            detail: {
+                href: anchor.href
+            }
         });
-    });
-}
+        anchor.dispatchEvent(event);
+    }
 
-export function getAnchors(rootElement) {
-    return rootElement.querySelectorAll('a[data-target-id]:not([data-target-id=""])');
-}
+    async function tryLoadContent(event) {
+        event.preventDefault();
+        const targetElement = getTargetElement(event.target);
+        !!targetElement
+            ? doLoadContent(event.target, targetElement)
+            : handleError(event.target);
+    }
 
-function dispatchBeforeLoadEvent(anchor) {
-    const event = new CustomEvent('hati:beforeLoad', {
-        bubbles: true,
-        cancelable: true,
-        detail: {
-            href: anchor.href
-        }
-    });
-    anchor.dispatchEvent(event);
-}
+    function getTargetElement(anchor) {
+        const targetElementId = anchor.getAttribute('data-target-id');
+        const targetElement = config.root.querySelector(`#${targetElementId}`);
+        return targetElement;
+    }
 
-async function tryLoadContent(event, anchor, rootElement, options) {
-    event.preventDefault();
-    const targetElement = getTargetElement(anchor, rootElement);
-    !!targetElement
-        ? doLoadContent(anchor, targetElement, options)
-        : handleError(anchor);
-}
+    async function doLoadContent(anchor, targetElement) {
+        const href = config?.router ? config.router(anchor.href) : anchor.href;
+        const response = await fetchContent(href);
+        renderContentInTargetElement(targetElement, response.content);
+        initialize({ ...config, root: targetElement });
+        dispatchContentLoadedEvent(targetElement, {
+            href: anchor.href,
+            responseStatusCode: response.statusCode
+        });
+    }
 
-function getTargetElement(anchor, rootElement) {
-    const targetElementId = anchor.getAttribute('data-target-id');
-    const targetElement = rootElement.querySelector(`#${targetElementId}`);
-    return targetElement;
-}
+    async function fetchContent(href) {
+        let response = await fetch(href);
+        const content = await response.text();
+        return {
+            content,
+            statusCode: response.status
+        };
+    }
 
-async function doLoadContent(anchor, targetElement, options) {
-    const href = options?.router ? options.router(anchor.href) : anchor.href;
-    const response = await fetchContent(href);
-    renderContentInTargetElement(targetElement, response.content);
-    initialize(targetElement);
-    dispatchContentLoadedEvent(targetElement, {
-        href: anchor.href,
-        responseStatusCode: response.statusCode
-    });
-}
+    function renderContentInTargetElement(targetElement, html) {
+        while (targetElement.lastChild)
+            targetElement.removeChild(targetElement.lastChild);
+        targetElement.insertAdjacentHTML('afterbegin', html);
+    }
 
-async function fetchContent(href) {
-    let response = await fetch(href);
-    const content = await response.text();
-    return {
-        content,
-        statusCode: response.status
-    };
-}
+    function dispatchContentLoadedEvent(targetElement, detail) {
+        const event = new CustomEvent('hati:DOMContentLoaded', {
+            bubbles: true,
+            cancelable: true,
+            detail
+        });
+        targetElement.dispatchEvent(event);
+    }
 
-function renderContentInTargetElement(targetElement, html) {
-    clearTargetElement(targetElement);
-    targetElement.insertAdjacentHTML('afterbegin', html);
-}
-
-function clearTargetElement(targetElement) {
-    while (targetElement.lastChild) {
-        targetElement.removeChild(targetElement.lastChild);
+    function handleError(anchor) {
+        const errorMessage = `No element found with id: ${anchor.getAttribute('data-target-id')}`;
+        const event = new CustomEvent('hati:error', {
+            bubbles: true,
+            cancelable: true,
+            detail: {
+                href: anchor.href,
+                errorMessage
+            }
+        });
+        anchor.dispatchEvent(event);
+        console.error(errorMessage);
     }
 }
 
-function dispatchContentLoadedEvent(targetElement, detail) {
-    const event = new CustomEvent('hati:DOMContentLoaded', {
-        bubbles: true,
-        cancelable: true,
-        detail
-    });
-    targetElement.dispatchEvent(event);
-}
-
-function handleError(anchor) {
-    const errorMessage = `No element found with id: ${anchor.getAttribute('data-target-id')}`;
-    const event = new CustomEvent('hati:error', {
-        bubbles: true,
-        cancelable: true,
-        detail: {
-            href: anchor.href,
-            errorMessage
-        }
-    });
-    anchor.dispatchEvent(event);
-    console.error(errorMessage);
+export function getAnchors(root) {
+    return root.querySelectorAll('a[data-target-id]:not([data-target-id=""])');
 }
--- test/function_initialize.spec.js
+++ test/function_initialize.spec.js
@@ -9,8 +9,8 @@ describe('function initialize', () => {
                 <p>This content will be replaced</p>
             </section>`;
 
-        return doTest(html, (finish, rootElement, event) => {
-            expect(rootElement.querySelector('#test-content').innerText).to.be.equal('Test content');
+        return doTest(html, (finish, root, event) => {
+            expect(root.querySelector('#test-content').innerText).to.be.equal('Test content');
             expect(event.detail.href).to.be.equal('http://localhost:9876/base/test/contents/test-content.html');
             expect(event.detail.responseStatusCode).to.be.equal(200);
             finish();
@@ -22,13 +22,13 @@ describe('function initialize', () => {
             <a href="/base/test/contents/nested.html" data-target-id="content">anchor</a>
             <section id="content"></section>`;
 
-        return doTest(html, (finish, rootElement) => {
-            rootElement.querySelector('#outer-content').addEventListener('hati:DOMContentLoaded', () => {
-                expect(rootElement.querySelector('#test-content').innerText).to.be.equal('Test content');
+        return doTest(html, (finish, root) => {
+            root.querySelector('#outer-content').addEventListener('hati:DOMContentLoaded', () => {
+                expect(root.querySelector('#test-content').innerText).to.be.equal('Test content');
                 finish();
             });
 
-            rootElement.querySelector('#content a').click();
+            root.querySelector('#content a').click();
         });
     });
 
@@ -37,66 +37,66 @@ describe('function initialize', () => {
             <a href="/base/test/contents/inexisting.html" data-target-id="content">anchor</a>
             <section id="content"></section>`;
 
-        return doTest(html, (finish, rootElement) => {
-            rootElement.querySelector('#content').addEventListener('hati:DOMContentLoaded', event => {
+        return doTest(html, (finish, root) => {
+            root.querySelector('#content').addEventListener('hati:DOMContentLoaded', event => {
                 expect(event.detail.responseStatusCode).to.be.equal(404);
                 finish();
             });
 
-            rootElement.querySelector('a').click();
+            root.querySelector('a').click();
         });
     });
 
     it('should dispatch a hati:beforeLoad event before trying to load content', () => {
-        const rootElement = document.createElement('div');
-        rootElement.innerHTML = `
+        const root = document.createElement('div');
+        root.innerHTML = `
             <a href="/base/test/contents/test-content.html" data-target-id="content">anchor</a>
             <div id="content"></div>`;
 
-        initialize(rootElement);
+        initialize({ root });
 
         return new Promise(resolve => {
-            rootElement.addEventListener('hati:beforeLoad', event => {
-                expect(event.target).to.be.equal(rootElement.querySelector('a'));
+            root.addEventListener('hati:beforeLoad', event => {
+                expect(event.target).to.be.equal(root.querySelector('a'));
                 expect(event.detail.href).to.be.equal('http://localhost:9876/base/test/contents/test-content.html');
                 resolve();
             });
 
-            rootElement.querySelector('a').click();
+            root.querySelector('a').click();
         });
     });
 
     it('should log an error in the console and dispatch a hati:error event if data-target-id resolves to no element', () => {
-        const rootElement = document.createElement('div');
-        rootElement.innerHTML = `<a href="/base/test/contents/test-content.html" data-target-id="non-existing-element">anchor</a>`;
+        const root = document.createElement('div');
+        root.innerHTML = `<a href="/base/test/contents/test-content.html" data-target-id="non-existing-element">anchor</a>`;
 
         console.error = sinon.fake();
 
-        initialize(rootElement);
+        initialize({ root });
 
         return new Promise(resolve => {
-            rootElement.addEventListener('hati:error', event => {
+            root.addEventListener('hati:error', event => {
                 expect(event.detail.href).to.be.equal('http://localhost:9876/base/test/contents/test-content.html');
                 expect(event.detail.errorMessage).to.be.equal('No element found with id: non-existing-element');
-                expect(event.target).to.be.equal(rootElement.querySelector('a'));
+                expect(event.target).to.be.equal(root.querySelector('a'));
                 resolve();
             });
 
-            rootElement.querySelector('a').click();
+            root.querySelector('a').click();
             expect(console.error.callCount).to.be.equal(1);
             expect(console.error.firstArg).to.be.equal('No element found with id: non-existing-element');
         });
     });
 
     it('should change the url without a page reload when an anchor is clicked', () => {
-        const rootElement = document.createElement('div');
-        rootElement.innerHTML = `
+        const root = document.createElement('div');
+        root.innerHTML = `
             <a href="/base/test/contents/test-content.html" data-target-id="content">anchor</a>
             <div id="content"></div>`;
 
-        initialize(rootElement);
+        initialize({ root });
 
-        rootElement.querySelector('a').click();
+        root.querySelector('a').click();
 
         expect(location.href).to.be.equal('http://localhost:9876/base/test/contents/test-content.html');
     });
@@ -108,8 +108,8 @@ describe('function initialize', () => {
                 <div id="content"></div>`;
             const router = sinon.spy(href => `${href}.html`);
 
-            return doTest(html, (finish, rootElement) => {
-                expect(rootElement.querySelector('#test-content').innerText).to.be.equal('Test content');
+            return doTest(html, (finish, root) => {
+                expect(root.querySelector('#test-content').innerText).to.be.equal('Test content');
                 expect(location.href).to.be.equal('http://localhost:9876/base/test/contents/test-content');
                 expect(router.callCount).to.be.equal(1);
                 finish();
@@ -118,20 +118,20 @@ describe('function initialize', () => {
     });
 });
 
-function doTest(html, testFunction, errorHandler, options) {
-    const rootElement = document.createElement('div');
-    rootElement.innerHTML = html;
+function doTest(html, testFunction, errorHandler, config) {
+    const root = document.createElement('div');
+    root.innerHTML = html;
 
-    initialize(rootElement, options ?? {});
+    initialize({ root, ...config });
 
     return new Promise(resolve => {
-        rootElement.addEventListener('hati:DOMContentLoaded', event => {
+        root.addEventListener('hati:DOMContentLoaded', event => {
             const finish = () => resolve();
-            testFunction(finish, rootElement, event);
+            testFunction(finish, root, event);
         });
 
         try {
-            rootElement.querySelector('a').click();
+            root.querySelector('a').click();
         } catch (error) {
             errorHandler(error);
         }
