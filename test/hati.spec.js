6047c5c7bed382c83c1044b0d97c12614f9bb067
André Teixeira <andremarcondesteixeira@gmail.com>
2021-04-11 00:32:16 -0300

    (REFACTOR) making sure all tests run, and in correct order

--- test/hati.spec.js
+++ test/hati.spec.js
@@ -16,7 +16,8 @@ function createRoot() {
     const root = document.createElement('div');
     root.innerHTML = `
         <a href="/base/test/contents/page1" id="anchor1" data-target-id="content" data-init>Page 1</a>
-        <a href="/base/test/contents/page3" id="anchor2" data-target-id="inexistent-section">Page 2 to inexistent section</a>
+        <!- the data-init attribute in the anchor below should be ignored -->
+        <a href="/base/test/contents/page3" id="anchor2" data-target-id="inexistent-section" data-init>Page 2 to inexistent section</a>
         <a href="/base/test/contents/inexistent" id="error404Anchor" data-target-id="content">Page 2</a>
         <nav data-anchors-target-id="content">
             <a href="/base/test/contents/page4" id="anchor4">Page 4</a>
@@ -30,43 +31,61 @@ function createRoot() {
 }
 
 function prepareTest(root, resolve) {
+    let amountOfTestsRunned = 0;
+
     root.addEventListener('hati:beforeLoad', event => {
         event.detail.matchUrl(/^.*\/page1$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(1);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page1`);
             expect(event.target).to.be.equal(root.querySelector('#anchor1'));
         });
 
         event.detail.matchUrl(/^.*\/page1\/page1-2$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(3);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page1/page1-2`);
             expect(event.target).to.be.equal(root.querySelector('#content a'));
         });
 
         event.detail.matchUrl(/^.*\/page2$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(5);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page2`);
             expect(event.target).to.be.equal(root.querySelector('#page-1-subcontent a'));
         });
 
         event.detail.matchUrl(/^.*\/page3$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(7);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page3`);
             expect(event.target).to.be.equal(root.querySelector('#anchor2'));
         });
 
         event.detail.matchUrl(/^.*\/inexistent$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(9);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/inexistent`);
             expect(event.target).to.be.equal(root.querySelector('#error404Anchor'));
         });
 
         event.detail.matchUrl(/^.*\/page4$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(11);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page4`);
             expect(event.target).to.be.equal(root.querySelector('#anchor4'));
         });
 
         event.detail.matchUrl(/^.*\/page5$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(13);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page5`);
             expect(event.target).to.be.equal(root.querySelector('#anchor5'));
         });
 
         event.detail.matchUrl(/^.*\/page6$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(15);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page6`);
             expect(event.target).to.be.equal(root.querySelector('#anchor6'));
         });
@@ -74,6 +93,8 @@ function prepareTest(root, resolve) {
 
     root.addEventListener('hati:DOMContentLoaded', event => {
         event.detail.matchUrl(/^.*\/page1$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(2);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page1`);
             expect(event.target).to.be.equal(root.querySelector('#content'));
             expect(event.detail.responseStatusCode).to.be.equal(200);
@@ -82,6 +103,8 @@ function prepareTest(root, resolve) {
         });
 
         event.detail.matchUrl(/^.*\/page1\/page1-2$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(4);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page1/page1-2`);
             expect(event.target).to.be.equal(root.querySelector('#page-1-subcontent'));
             expect(event.detail.responseStatusCode).to.be.equal(200);
@@ -90,6 +113,8 @@ function prepareTest(root, resolve) {
         });
 
         event.detail.matchUrl(/^.*\/page2$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(6);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page2`);
             expect(event.target).to.be.equal(root.querySelector('#content'));
             expect(event.detail.responseStatusCode).to.be.equal(200);
@@ -98,6 +123,8 @@ function prepareTest(root, resolve) {
         });
 
         event.detail.matchUrl(/^.*\/inexistent$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(10);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/inexistent`);
             expect(event.target).to.be.equal(root.querySelector('#content'));
             expect(event.detail.responseStatusCode).to.be.equal(404);
@@ -106,6 +133,8 @@ function prepareTest(root, resolve) {
         });
 
         event.detail.matchUrl(/^.*\/page4$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(12);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page4`);
             expect(event.target).to.be.equal(root.querySelector('#content'));
             expect(event.detail.responseStatusCode).to.be.equal(200);
@@ -114,6 +143,8 @@ function prepareTest(root, resolve) {
         });
 
         event.detail.matchUrl(/^.*\/page5$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(14);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page5`);
             expect(event.target).to.be.equal(root.querySelector('#content'));
             expect(event.detail.responseStatusCode).to.be.equal(200);
@@ -122,6 +153,8 @@ function prepareTest(root, resolve) {
         });
 
         event.detail.matchUrl(/^.*\/page6$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(16);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page6`);
             expect(event.target).to.be.equal(root.querySelector('#content-2'));
             expect(event.detail.responseStatusCode).to.be.equal(200);
@@ -132,6 +165,8 @@ function prepareTest(root, resolve) {
 
     root.addEventListener('hati:error', event => {
         event.detail.matchUrl(/^.*\/page3$/, () => {
+            amountOfTestsRunned++;
+            expect(amountOfTestsRunned).to.be.equal(8);
             expect(event.detail.href).to.be.equal(`http://localhost:9876/base/test/contents/page3`);
             expect(event.target).to.be.equal(root.querySelector('#anchor2'));
             expect(event.detail.errorMessage).to.be.equal('No element found with id: inexistent-section');
