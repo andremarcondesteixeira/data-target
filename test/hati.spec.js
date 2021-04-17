03ac245229652da328243ed28394e9660e36e4e4
André Teixeira <andremarcondesteixeira@gmail.com>
2021-04-17 16:47:51 -0300

    (REFACTOR) refactors tests

--- test/hati.spec.js
+++ test/hati.spec.js
@@ -91,84 +91,95 @@ function prepareTest(root, finish, urlSuffix = '') {
     let page3Visits = 0;
 
     root.addEventListener('hati:DOMContentLoaded', event => {
-        event.detail.matchUrl(new RegExp(`^.+\/page1${regexSuffix(urlSuffix)}$`), () => {
-            amountOfTestsRunned++;
-            expect(amountOfTestsRunned).to.be.equal(1);
-            let url = `http://localhost:9876/base/test/contents/page1${assertionSuffix(urlSuffix)}`;
-            expect(event.detail.url).to.be.equal(url);
-            expect(event.target).to.be.equal(root.querySelector('#content'));
-            expect(event.detail.responseStatusCode).to.be.equal(200);
-            expect(root.querySelector('#content .content').innerText).to.be.equal('page 1');
-            root.querySelector('#content a').click();
-        });
-
-        event.detail.matchUrl(new RegExp(`^.+\/page1\/page1-2${regexSuffix(urlSuffix)}$`), () => {
-            amountOfTestsRunned++;
-            expect(amountOfTestsRunned).to.be.equal(2);
-            let url = `http://localhost:9876/base/test/contents/page1/page1-2${assertionSuffix(urlSuffix)}`;
-            expect(event.detail.url).to.be.equal(url);
-            expect(event.target).to.be.equal(root.querySelector('#page-1-subcontent'));
-            expect(event.detail.responseStatusCode).to.be.equal(200);
-            expect(root.querySelector('#page-1-subcontent .content').innerText).to.be.equal('page 1-2');
-            root.querySelector('#page-1-subcontent a').click();
-        });
-
-        event.detail.matchUrl(new RegExp(`^.+\/page2${urlSuffixRegex(urlSuffix)}$`), () => {
-            amountOfTestsRunned++;
-            expect(amountOfTestsRunned).to.be.equal(3);
-            let url = `http://localhost:9876/base/test/contents/page2${urlSuffix}`;
-            expect(event.detail.url).to.be.equal(url);
-            expect(event.target).to.be.equal(root.querySelector('#content'));
-            expect(event.detail.responseStatusCode).to.be.equal(200);
-            expect(root.querySelector('#content .content').innerText).to.be.equal('page 2');
-            root.querySelector('#anchor3').click();
-        });
-
-        event.detail.matchUrl(new RegExp(`^.+\/page3${urlSuffixRegex(urlSuffix)}$`), () => {
-            let url = `http://localhost:9876/base/test/contents/page3${urlSuffix}`;
-            expect(event.detail.url).to.be.equal(url);
-            expect(event.target).to.be.equal(root.querySelector('#content'));
-            expect(event.detail.responseStatusCode).to.be.equal(200);
-            expect(root.querySelector('#content .content').innerText).to.be.equal('page 3');
-
-            amountOfTestsRunned++;
-            page3Visits++;
-            if (page3Visits === 2) {
-                expect(amountOfTestsRunned).to.be.equal(6);
-                root.querySelector('#error404anchor').click();
-            } else {
-                expect(amountOfTestsRunned).to.be.equal(4);
-                root.querySelector('#anchor4').click();
-            }
-        });
-
-        event.detail.matchUrl(new RegExp(`^.+\/page4${urlSuffixRegex(urlSuffix)}$`), () => {
-            amountOfTestsRunned++;
-            expect(amountOfTestsRunned).to.be.equal(5);
-            let url = `http://localhost:9876/base/test/contents/page4${urlSuffix}`;
-            expect(event.detail.url).to.be.equal(url);
-            expect(event.target).to.be.equal(root.querySelector('#content-2'));
-            expect(event.detail.responseStatusCode).to.be.equal(200);
-            expect(root.querySelector('#content-2 .content').innerText).to.be.equal('page 4');
-            history.back();
-        });
-
-        event.detail.matchUrl(new RegExp(`^.+\/error404${urlSuffixRegex(urlSuffix)}$`), () => {
-            amountOfTestsRunned++;
-            expect(amountOfTestsRunned).to.be.equal(7);
-            let url = `http://localhost:9876/base/test/contents/error404${urlSuffix}`;
-            expect(event.detail.url).to.be.equal(url);
-            expect(event.target).to.be.equal(root.querySelector('#content'));
-            expect(event.detail.responseStatusCode).to.be.equal(404);
-            expect(root.querySelector('#content').innerText).to.be.equal('NOT FOUND');
-            let errorCalled = false;
-            console.error = sinon.stub().callsFake(() => errorCalled = true);
-            root.querySelector('#targetError').click();
-            expect(errorCalled).to.be.true;
-            sinon.restore();
-            finish();
-        });
+        event.detail.matchUrl(new RegExp(`^.+\/page1${regexSuffix(urlSuffix)}$`), () => step1(event));
+        event.detail.matchUrl(new RegExp(`^.+\/page1\/page1-2${regexSuffix(urlSuffix)}$`), () => step2(event));
+        event.detail.matchUrl(new RegExp(`^.+\/page2${urlSuffixRegex(urlSuffix)}$`), () => step3(event));
+        event.detail.matchUrl(new RegExp(`^.+\/page3${urlSuffixRegex(urlSuffix)}$`), () => steps4and6(event));
+        event.detail.matchUrl(new RegExp(`^.+\/page4${urlSuffixRegex(urlSuffix)}$`), () => step5(event));
+        event.detail.matchUrl(new RegExp(`^.+\/error404${urlSuffixRegex(urlSuffix)}$`), () => step7(event));
     });
+
+    function step1(event) {
+        amountOfTestsRunned++;
+        expect(amountOfTestsRunned).to.be.equal(1);
+        let url = `http://localhost:9876/base/test/contents/page1${assertionSuffix(urlSuffix)}`;
+        expect(event.detail.url).to.be.equal(url);
+        expect(event.target).to.be.equal(root.querySelector('#content'));
+        expect(event.detail.responseStatusCode).to.be.equal(200);
+        expect(root.querySelector('#content .content').innerText).to.be.equal('page 1');
+        root.querySelector('#content a').click();
+    }
+
+    function step2(event) {
+        amountOfTestsRunned++;
+        expect(amountOfTestsRunned).to.be.equal(2);
+        let url = `http://localhost:9876/base/test/contents/page1/page1-2${assertionSuffix(urlSuffix)}`;
+        expect(event.detail.url).to.be.equal(url);
+        expect(event.target).to.be.equal(root.querySelector('#page-1-subcontent'));
+        expect(event.detail.responseStatusCode).to.be.equal(200);
+        expect(root.querySelector('#page-1-subcontent .content').innerText).to.be.equal('page 1-2');
+        root.querySelector('#page-1-subcontent a').click();
+    }
+
+    function step3(event) {
+        amountOfTestsRunned++;
+        expect(amountOfTestsRunned).to.be.equal(3);
+        let url = `http://localhost:9876/base/test/contents/page2${urlSuffix}`;
+        expect(event.detail.url).to.be.equal(url);
+        expect(event.target).to.be.equal(root.querySelector('#content'));
+        expect(event.detail.responseStatusCode).to.be.equal(200);
+        expect(root.querySelector('#content .content').innerText).to.be.equal('page 2');
+        root.querySelector('#anchor3').click();
+    }
+
+    function steps4and6(event) {
+        let url = `http://localhost:9876/base/test/contents/page3${urlSuffix}`;
+        expect(event.detail.url).to.be.equal(url);
+        expect(event.target).to.be.equal(root.querySelector('#content'));
+        expect(event.detail.responseStatusCode).to.be.equal(200);
+        expect(root.querySelector('#content .content').innerText).to.be.equal('page 3');
+
+        amountOfTestsRunned++;
+        page3Visits++;
+        if (page3Visits === 2) {
+            expect(amountOfTestsRunned).to.be.equal(6);
+            root.querySelector('#error404anchor').click();
+        } else {
+            expect(amountOfTestsRunned).to.be.equal(4);
+            root.querySelector('#anchor4').click();
+        }
+    }
+
+    function step5(event) {
+        amountOfTestsRunned++;
+        expect(amountOfTestsRunned).to.be.equal(5);
+        let url = `http://localhost:9876/base/test/contents/page4${urlSuffix}`;
+        expect(event.detail.url).to.be.equal(url);
+        expect(event.target).to.be.equal(root.querySelector('#content-2'));
+        expect(event.detail.responseStatusCode).to.be.equal(200);
+        expect(root.querySelector('#content-2 .content').innerText).to.be.equal('page 4');
+        history.back();
+    }
+
+    function step7(event) {
+        amountOfTestsRunned++;
+        expect(amountOfTestsRunned).to.be.equal(7);
+        let url = `http://localhost:9876/base/test/contents/error404${urlSuffix}`;
+        expect(event.detail.url).to.be.equal(url);
+        expect(event.target).to.be.equal(root.querySelector('#content'));
+        expect(event.detail.responseStatusCode).to.be.equal(404);
+        expect(root.querySelector('#content').innerText).to.be.equal('NOT FOUND');
+        step8();
+    }
+
+    function step8() {
+        let errorCalled = false;
+        console.error = sinon.stub().callsFake(() => errorCalled = true);
+        root.querySelector('#targetError').click();
+        expect(errorCalled).to.be.true;
+        sinon.restore();
+        finish();
+    }
 }
 
 function regexSuffix(urlSuffix = '') {
