4104d394b132572c619f5daa8f4307384ba793b7
André Teixeira <andremarcondesteixeira@gmail.com>
2021-04-17 23:37:51 -0300

    (REFACTOR) refactors test

--- test/hati-unhappy-path.spec.js
+++ test/hati-unhappy-path.spec.js
@@ -4,19 +4,20 @@ describe('the unhappy path of hati', () => {
     it('should treat all HTTP statuses equally by default', () => new Promise(resolve => {
         const root = document.createElement('div');
         root.innerHTML = `
-            <a href="/base/test/contents/unhappy-inexistent-page.html" data-target-id="content" data-init>Inexistent Page</a>
-            <div id="content"></div>
+            <a href="/base/test/contents/unhappy-path-inexistent-page.html"
+               data-target-id="unhappy-path-test1-content"
+               data-init>Inexistent Page</a>
+            <div id="unhappy-path-test1-content"></div>
         `;
         hati(root);
-        root.querySelector('#content').addEventListener('hati:DOMContentLoaded', event => {
-            event.detail.matchUrl(/^.+\/unhappy-inexistent-page\.html$/, () => {
-                let url = `http://localhost:9876/base/test/contents/unhappy-inexistent-page.html`;
-                expect(event.detail.url).to.be.equal(url);
-                expect(event.target).to.be.equal(root.querySelector('#content'));
-                expect(event.detail.responseStatusCode).to.be.equal(404);
-                expect(root.querySelector('#content').innerText).to.be.equal('NOT FOUND');
-                resolve();
-            });
-        });
+        const url = /^.+\/unhappy-path-inexistent-page\.html$/;
+        root.addEventListener('hati:DOMContentLoaded', event => event.detail.matchUrl(url, () => {
+            let url = `http://localhost:9876/base/test/contents/unhappy-path-inexistent-page.html`;
+            expect(event.detail.url).to.be.equal(url);
+            expect(event.target).to.be.equal(root.querySelector('#unhappy-path-test1-content'));
+            expect(event.detail.responseStatusCode).to.be.equal(404);
+            expect(root.querySelector('#unhappy-path-test1-content').innerText).to.be.equal('NOT FOUND');
+            resolve();
+        }));
     }));
 });
