e137a432eccf1c6d72461323dc2f7950f7853391
André Teixeira <andremarcondesteixeira@gmail.com>
2021-04-18 01:07:36 -0300

    (GREEN) tests missing target element error, when there is no provided error handler

--- test/hati-unhappy-path.spec.js
+++ test/hati-unhappy-path.spec.js
@@ -20,4 +20,19 @@ describe('the unhappy path of hati', () => {
             resolveTest1();
         }));
     }));
+
+    it('should use the default error handler, if no error handler is provided, when target element does not exist', () => {
+        const test2Root = document.createElement('div');
+        test2Root.innerHTML = `
+            <a href="/base/test/contents/unhappy-path/test2.html"
+               data-target-id="unhappy-path-test2-inexistent-target"
+               data-init>Inexistent Page</a>
+        `;
+        let error;
+        console.error = sinon.stub().callsFake(e => error = e);
+        hati(test2Root);
+        expect(console.error.callCount).to.be.equal(1);
+        expect(error.message).to.be.equal('No element found with id: "unhappy-path-test2-inexistent-target"');
+        sinon.restore();
+    });
 });
