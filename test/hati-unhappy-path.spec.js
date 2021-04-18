49a9279e840f8c9793d24f490cd14215a71479fe
André Teixeira <andremarcondesteixeira@gmail.com>
2021-04-18 01:09:05 -0300

    (REFACTOR) refactors tests

--- test/hati-unhappy-path.spec.js
+++ test/hati-unhappy-path.spec.js
@@ -1,36 +1,36 @@
 import hati from '../src/hati.js';
 
 describe('the unhappy path of hati', () => {
-    it('should treat all HTTP statuses equally by default', () => new Promise(resolveTest1 => {
-        const test1Root = document.createElement('div');
-        test1Root.innerHTML = `
+    it('should treat all HTTP statuses equally by default', () => new Promise(resolve => {
+        const root = document.createElement('div');
+        root.innerHTML = `
             <a href="/base/test/contents/unhappy-path-inexistent-page.html"
                data-target-id="unhappy-path-test1-content"
                data-init>Inexistent Page</a>
             <div id="unhappy-path-test1-content"></div>
         `;
-        hati(test1Root);
-        const test1Url = /^.+\/unhappy-path-inexistent-page\.html$/;
-        test1Root.addEventListener('hati:DOMContentLoaded', test1Event => test1Event.detail.matchUrl(test1Url, () => {
-            let expectedTest1Url = `http://localhost:9876/base/test/contents/unhappy-path-inexistent-page.html`;
-            expect(test1Event.detail.url).to.be.equal(expectedTest1Url);
-            expect(test1Event.target).to.be.equal(test1Root.querySelector('#unhappy-path-test1-content'));
-            expect(test1Event.detail.responseStatusCode).to.be.equal(404);
-            expect(test1Root.querySelector('#unhappy-path-test1-content').innerText).to.be.equal('NOT FOUND');
-            resolveTest1();
+        hati(root);
+        const urlRegex = /^.+\/unhappy-path-inexistent-page\.html$/;
+        root.addEventListener('hati:DOMContentLoaded', event => event.detail.matchUrl(urlRegex, () => {
+            let url = `http://localhost:9876/base/test/contents/unhappy-path-inexistent-page.html`;
+            expect(event.detail.url).to.be.equal(url);
+            expect(event.target).to.be.equal(root.querySelector('#unhappy-path-test1-content'));
+            expect(event.detail.responseStatusCode).to.be.equal(404);
+            expect(root.querySelector('#unhappy-path-test1-content').innerText).to.be.equal('NOT FOUND');
+            resolve();
         }));
     }));
 
     it('should use the default error handler, if no error handler is provided, when target element does not exist', () => {
-        const test2Root = document.createElement('div');
-        test2Root.innerHTML = `
+        const root = document.createElement('div');
+        root.innerHTML = `
             <a href="/base/test/contents/unhappy-path/test2.html"
                data-target-id="unhappy-path-test2-inexistent-target"
                data-init>Inexistent Page</a>
         `;
         let error;
         console.error = sinon.stub().callsFake(e => error = e);
-        hati(test2Root);
+        hati(root);
         expect(console.error.callCount).to.be.equal(1);
         expect(error.message).to.be.equal('No element found with id: "unhappy-path-test2-inexistent-target"');
         sinon.restore();
