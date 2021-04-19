86c9e7fa01104fee4ec577e67e517ad2189be248
André Teixeira <andremarcondesteixeira@gmail.com>
2021-04-19 00:00:03 -0300

    (GREEN) testing data-target-id

--- test/happy-path/hati-happy-path.spec.js
+++ test/happy-path/hati-happy-path.spec.js
@@ -12,5 +12,17 @@ describe('hati unhappy path', () => {
             <a href="/base/test/happy-path/test1.html" data-target-id="content">Test 1</a>
             <div id="content"><div>
         `;
+
+        hati(root);
+        root.addEventListener('hati:DOMContentLoaded', event => event.detail.matchUrl(/^.+\/happy-path\/test1\.html$/, () => {
+            console.log(event.detail);
+            expect(event.target).to.be.equal(root.querySelector('#content'));
+            expect(event.detail.url).to.be.equal('http://localhost:9876/base/test/happy-path/test1.html');
+            expect(event.detail.responseStatusCode).to.be.equal(200);
+            expect(root.querySelector('#content').innerText.trim()).to.be.equal('Test 1');
+            resolve();
+        }));
+
+        root.querySelector('a').click();
     }));
 });
