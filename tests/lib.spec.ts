75d393a83985f79021d0ff21faf2a4165c23774d
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-08-15 01:33:57 -0300

    trying to write a test with playwright

--- tests/lib.spec.ts
+++ tests/lib.spec.ts
@@ -2,17 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
     await page.goto(`${process.env.URL}`);
-
     await page.setContent(`
         <div id="content"></div>
         <a href="/tests/content.html" data-target-id="content" data-init>load</a>
     `);
-
     await page.addScriptTag({ type: 'module', url: `${process.env.URL}/dist/lib.js` });
-
-    expect(true).toBe(true);
-
-    page.on('requestfinished', request => {
-        console.log(request);
-    });
+    await page.click('a');
+    const contentReceiver = await page.$('#content');
+    const loadedContent = await contentReceiver.$('#loaded-content');
+    const loadedText = await loadedContent.innerText();
+    expect(loadedText).toBe('loaded content');
 });
