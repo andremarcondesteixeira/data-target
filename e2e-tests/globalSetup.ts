cea671e030089d2755159f3368e944796a3deeef
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-08-29 01:41:04 -0300

    creates a DEBUG_MODE env var

--- e2e-tests/globalSetup.ts
+++ e2e-tests/globalSetup.ts
@@ -4,7 +4,9 @@ import useragent from 'express-useragent';
 import getPort from 'get-port';
 
 export default async function globalSetup() {
-    console.info('starting server');
+    const isDebugMode = process.env.DEBUG_MODE === '1';
+
+    isDebugMode && console.info('starting server');
 
     const port = await getPort();
     process.env.URL = `http://${process.env.HOST}:${port}`;
@@ -12,18 +14,18 @@ export default async function globalSetup() {
     const server = express()
         .use(useragent.express())
         .use((req, res, next) => {
-            console.info(`${req.useragent?.browser} -> ${req.method} ${req.url} -> ${res.statusCode}`);
+            isDebugMode && console.info(`${req.useragent?.browser} -> ${req.method} ${req.url} -> ${res.statusCode}`);
             next();
         })
         .use(express.static(__dirname))
         .listen(port, process.env.HOST, () => {
-            console.info(`server listening at ${process.env.URL}`);
+            isDebugMode && console.info(`server listening at ${process.env.URL}`);
         });
 
     return async () => {
         await new Promise(done => {
             server.close(done);
-            console.info('server stopped');
+            isDebugMode && console.info('server stopped');
         });
     };
 }
