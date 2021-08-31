2ff85677f504311867d90a22fbc535f1b27d895a
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-08-31 01:29:30 -0300

    refactors tests

--- e2e-tests/hyperlinksPlusPlus.spec.ts
+++ e2e-tests/hyperlinksPlusPlus.spec.ts
@@ -1,15 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { expect, Page, PlaywrightTestArgs, PlaywrightTestOptions, PlaywrightWorkerArgs, PlaywrightWorkerOptions, test, TestInfo } from '@playwright/test';
 
 test.describe('basic functionality', () => {
-    test('clicking an anchor with a data-target-id attribute will load the content inside the element whose id matches the attribute', async ({ page }) => {
-        await page.goto(`${process.env['URL']}`);
-        await page.setContent(`
+    test('clicking an anchor with a data-target-id attribute will load the content inside the element whose id matches the attribute', prepare({
+        pageContent: `
             <a id="link" href="content.html" data-target="content">load</a>
             <div id="content"></div>
-        `);
-        await page.addScriptTag({ type: 'module', url: `${process.env['URL']}/build/hyperlinksPlusPlus.js` });
-
-        page.on('load', async () => {
+        `,
+        assertions: async page => {
             await page.click('#link');
 
             const content = await page.$('#content');
@@ -19,18 +16,15 @@ test.describe('basic functionality', () => {
             const loadedContent = await content?.waitForSelector('#loaded-content');
             const loadedContentText = await loadedContent?.innerText();
             expect(loadedContentText).toBe('loaded content');
-        });
-    });
+        }
+    }));
 
-    test('an anchor with a data-init atribute will load automatically', async ({ page }) => {
-        page.on("console", message => console.log(message));
-        await page.goto(`${process.env['URL']}`);
-        await page.setContent(`
+    test('an anchor with a data-init atribute will load automatically', prepare({
+        pageContent: `
             <a id="link" href="content.html" data-target="content" data-init>load</a>
             <div id="content"></div>
-        `);
-        await page.addScriptTag({ type: 'module', url: `${process.env['URL']}/build/hyperlinksPlusPlus.js` });
-        page.on('load', async () => {
+        `,
+        assertions: async page => {
             const content = await page.$('#content');
             let contentText = (await content?.innerText())?.toLowerCase();
             expect(contentText).not.toMatch(/(404)|(error)|(not found)|(enoent)|(no such file)/);
@@ -38,6 +32,27 @@ test.describe('basic functionality', () => {
             const loadedContent = await content?.waitForSelector('#loaded-content');
             const loadedContentText = await loadedContent?.innerText();
             expect(loadedContentText).toBe('loaded content');
-        });
-    });
+        }
+    }));
 });
+
+function prepare(config: TestConfig): PrepareReturn {
+    return async ({ page }: PrepareArgs): Promise<void> => {
+        await page.goto(`${process.env['URL']}`);
+        await page.setContent(config.pageContent)
+        await page.addScriptTag({ type: 'module', url: `${process.env['URL']}/build/hyperlinksPlusPlus.js` })
+        page.on('load', () => config.assertions(page));
+    };
+}
+
+interface TestConfig {
+    pageContent: string;
+    assertions: (page: Page) => void;
+}
+
+type PrepareReturn = (args: PrepareArgs, testInfo: TestInfo) => void | Promise<void>;
+
+type PrepareArgs = PlaywrightTestArgs
+    & PlaywrightTestOptions
+    & PlaywrightWorkerArgs
+    & PlaywrightWorkerOptions;
