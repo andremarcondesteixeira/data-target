dba7bddf71366626f475efb31409119d3d3a24a0
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-09-05 12:39:50 -0300

    refactors tests

--- e2e-tests/hyperlinksPlusPlus.spec.ts
+++ e2e-tests/hyperlinksPlusPlus.spec.ts
@@ -3,26 +3,26 @@ import fs from 'fs';
 import path from 'path';
 
 test.describe('basic functionality', () => {
-    test('clicking an anchor with a data-target attribute will load the content inside the element whose id matches the attribute', prepare({
+    test('clicking an anchor with a "data-target" attribute will put the response of the http request inside the element with a matching "id" attribute', prepare({
         pageContent: /*html*/ `
-            <a id="link" href="/pages/page1.html" data-target="content">click me!</a>
-            <div id="content"></div>
+            <a id="hyperlink" href="/pages/basic.html" data-target="target-element-id">click me!</a>
+            <div id="target-element-id"></div>
         `,
         assertions: async page => {
-            await page.click('#link');
-            await at(page).assertTarget('content').receivedContentFromFile('pages/page1.html');
+            await page.click('#hyperlink');
+            await at(page).assertTarget('target-element-id').receivedContentFromFile('pages/basic.html');
         }
     }));
 
-    test('an anchor with a data-autoload atribute loads automatically', prepare({
+    test('an anchor with a "data-autoload" atribute loads automatically', prepare({
         pageContent: /*html*/ `
-            <a href="/pages/page1.html" data-target="content" data-autoload>
-                I will make the request by myself after the page is rendered, but just 1 time
+            <a href="/pages/basic-automatic.html" data-target="will-get-content-automatically" data-autoload>
+                This anchor will dispatch the request automatically after initial page load
             </a>
-            <div id="content"></div>
+            <div id="will-get-content-automatically"></div>
         `,
         assertions: async page => {
-            await at(page).assertTarget('content').receivedContentFromFile('pages/page1.html');
+            await at(page).assertTarget('will-get-content-automatically').receivedContentFromFile('pages/basic-automatic.html');
         }
     }));
 
@@ -33,7 +33,7 @@ test.describe('basic functionality', () => {
                     <section class="books">
                         <ul>
                             <li>
-                                <a id="nested-anchor" href="/pages/page1.html" data-target="main-content">
+                                <a id="nested-anchor" href="/pages/requested-from-nested-anchor.html" data-target="is-upper-in-the-dom-tree">
                                     Anchors should work irrespective of where they are in the DOM
                                 </a>
                             </li>
@@ -41,11 +41,11 @@ test.describe('basic functionality', () => {
                     </section>
                 </nav>
             </header>
-            <main id="main-content"></main>
+            <main id="is-upper-in-the-dom-tree"></main>
         `,
         assertions: async page => {
             await page.click('#nested-anchor');
-            await at(page).assertTarget('main-content').receivedContentFromFile('pages/page1.html');
+            await at(page).assertTarget('is-upper-in-the-dom-tree').receivedContentFromFile('pages/requested-from-nested-anchor.html');
         }
     }));
 });
