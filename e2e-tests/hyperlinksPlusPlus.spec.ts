919c6f9fb0476f9aa84aba44248e1a75eb0af399
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-09-10 15:51:03 -0300

    code formatting

--- e2e-tests/hyperlinksPlusPlus.spec.ts
+++ e2e-tests/hyperlinksPlusPlus.spec.ts
@@ -1,9 +1,13 @@
 import test from './customFixtures';
 
 test.describe('basic functionality', () => {
-    test('clicking an anchor with a "data-target" attribute will put the response of the http request inside the element with a matching "id" attribute', async ({ withPageContent }) => {
+    test('an anchor with a "data-target" attribute puts the response of the http request in the element with a matching id', async ({
+        withPageContent
+    }) => {
         const html = /*html*/ `
-            <a id="hyperlink" href="/pages/basic.html" data-target="target-element-id">click me!</a>
+            <a id="hyperlink"
+               href="/pages/basic.html"
+               data-target="target-element-id">click me!</a>
             <div id="target-element-id"></div>
         `;
 
@@ -14,14 +18,18 @@ test.describe('basic functionality', () => {
             .test();
     });
 
-    test('clicking an anchor with a data-target attribute works independently of the nesting level of the anchor', async ({ withPageContent }) => {
+    test('an anchor with a data-target attribute works independently of the nesting level of the anchor', async ({
+        withPageContent
+    }) => {
         const html = /*html*/ `
             <header>
                 <nav>
                     <section class="books">
                         <ul>
                             <li>
-                                <a id="nested-anchor" href="/pages/requested-from-nested-anchor.html" data-target="is-upper-in-the-dom-tree">
+                                <a id="nested-anchor"
+                                   href="/pages/requested-from-nested-anchor.html"
+                                   data-target="is-upper-in-the-dom-tree">
                                     Anchors should work no matter where they are in the DOM
                                 </a>
                             </li>
@@ -39,9 +47,13 @@ test.describe('basic functionality', () => {
             .test();
     });
 
-    test('an anchor with a "data-autoload" attribute loads automatically', async ({ withPageContent }) => {
+    test('an anchor with a "data-autoload" attribute loads automatically', async ({
+        withPageContent
+    }) => {
         const html = /*html*/ `
-            <a href="/pages/basic-automatic.html" data-target="will-get-content-automatically" data-autoload>
+            <a href="/pages/basic-automatic.html"
+               data-target="will-get-content-automatically"
+               data-autoload>
                 This anchor will dispatch the request automatically after initial page load
             </a>
             <div id="will-get-content-automatically"></div>
@@ -53,15 +65,19 @@ test.describe('basic functionality', () => {
             .test();
     });
 
-    test('an anchor with a "data-autoload" attribute loads automatically independently of the nesting level of the anchor', async ({ withPageContent }) => {
+    test(`an anchor with a "data-autoload" attribute loads automatically independently of the anchor's nesting level`, async ({
+        withPageContent
+    }) => {
         const html = /*html*/ `
             <header>
                 <nav>
                     <section class="books">
                         <ul>
                             <li>
-                                <a href="/pages/requested-from-nested-anchor.html" data-target="is-upper-in-the-dom-tree" data-autoload>
-                                   Autoload anchors should work no matter where they are in the DOM
+                                <a href="/pages/requested-from-nested-anchor.html"
+                                   data-target="is-upper-in-the-dom-tree"
+                                   data-autoload>
+                                    Autoload anchors should work no matter where they are in the DOM
                                 </a>
                             </li>
                         </ul>
@@ -77,10 +93,16 @@ test.describe('basic functionality', () => {
             .test();
     });
 
-    test('Multiple autoload anchors can be defined simultaneously as long as they do not point to the same target', async ({ withPageContent }) => {
+    test('Multiple autoload anchors can be defined simultaneously as long as they do not point to the same target', async ({
+        withPageContent
+    }) => {
         const html = /* html */ `
-            <a href="/pages/basic.html" data-target="content-1" data-autoload>Loads automatically inside #content-1</a>
-            <a href="/pages/basic-automatic.html" data-target="content-2" data-autoload>Loads automatically inside #content-2</a>
+            <a href="/pages/basic.html"
+               data-target="content-1"
+               data-autoload>Loads automatically inside #content-1</a>
+            <a href="/pages/basic-automatic.html"
+               data-target="content-2"
+               data-autoload>Loads automatically inside #content-2</a>
             <div id="content-1"></div>
             <div id="content-2"></div>
         `;
