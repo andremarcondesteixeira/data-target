b94adefe15b76f868cecad663ead016bc22d3d9e
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-12-03 10:18:15 -0300

    refactoring

--- e2e-tests/customFixtures/withPageContentFixture.ts
+++ e2e-tests/customFixtures/withPageContentFixture.ts
@@ -37,112 +37,117 @@ export default async function withPageContent(
     use: (r: (html: string) => WithPageContentFixture) => Promise<void>
 ): Promise<void> {
     await use((html: string): WithPageContentFixture => {
-        return createFixture(html,  prepareContext, createEventLogger);
+        return new WithPageContentFixtureFactory(html,  prepareContext, createEventLogger).create();
     });
 }
 
-function createFixture(
-    html: string,
-    prepareContext: (args: PrepareContextFixtureArgs) => Promise<Page>,
-    createEventLogger: (page: Page) => Promise<EventLogger>
-) {
-    const actions: PageConsumer[] = [];
-    const assertions: ((page: Page, eventLogger: EventLogger) => Promise<void>)[] = [];
+class WithPageContentFixtureFactory {
+    private actions: PageConsumer[] = [];
+    private assertions: ((page: Page, eventLogger: EventLogger) => Promise<void>)[] = [];
 
-    const assertionChainStart: WithPageContentFixtureFirstAssertion = {
-        expectThat: function () {
-            const chain = {
-                and: () => ({
-                    ...this,
-                    runTest,
-                }),
-            };
+    constructor(
+        private html: string,
+        private prepareContext: (args: PrepareContextFixtureArgs) => Promise<Page>,
+        private createEventLogger: (page: Page) => Promise<EventLogger>
+    ) { }
 
-            return {
-                element: (selector: string) => ({
-                    hasSameContentOf: (filename: string) => {
-                        assertions.push(async (page: Page, eventLogger: EventLogger) => {
-                            const targetSelector = await page.evaluate(args => {
-                                const [html, elementSelector] = args;
-                                const div = window.document.createElement('div');
-                                div.insertAdjacentHTML('afterbegin', html);
-                                const desiredTarget = div.querySelector(elementSelector);
-                                const anchors = Array.from(div.querySelectorAll('a[data-target]'));
-                                const targetSelector = anchors.map(a => {
-                                    return a.getAttribute('data-target');
-                                }).filter(targetSelector => {
-                                    const targetForThisAnchor = div.querySelector(targetSelector);
-                                    return targetForThisAnchor === desiredTarget;
-                                })[0];
-                                return targetSelector;
-                            }, [html, selector]);
+    create() {
+        const self = this;
+        const assertionChainStart: WithPageContentFixtureFirstAssertion = {
+            expectThat: function () {
+                const chain = {
+                    and: () => ({
+                        ...assertionChainStart,
+                       runTest: self.runTest.bind(self),
+                    }),
+                };
 
-                            await waitUntilTargetElementHasReceivedContent(targetSelector, filename, eventLogger);
+                return {
+                    element: (selector: string) => ({
+                        hasSameContentOf: (filename: string) => {
+                            self.assertions.push(async (page: Page, eventLogger: EventLogger) => {
+                                const targetSelector = await page.evaluate(args => {
+                                    const [html, elementSelector] = args;
+                                    const div = window.document.createElement('div');
+                                    div.insertAdjacentHTML('afterbegin', html);
+                                    const desiredTarget = div.querySelector(elementSelector);
+                                    const anchors = Array.from(div.querySelectorAll('a[data-target]'));
+                                    const targetSelector = anchors.map(a => {
+                                        return a.getAttribute('data-target');
+                                    }).filter(targetSelector => {
+                                        const targetForThisAnchor = div.querySelector(targetSelector);
+                                        return targetForThisAnchor === desiredTarget;
+                                    })[0];
+                                    return targetSelector;
+                                }, [self.html, selector]);
 
-                            const targetElement = await page.$(selector);
-                            const actualInnerHTML = (await targetElement.innerHTML()).trim();
-                            const expectedInnerHTML = (await readFileContent(filename)).trim();
-                            expect(actualInnerHTML).toBe(expectedInnerHTML);
+                                await waitUntilTargetElementHasReceivedContent(targetSelector, filename, eventLogger);
+
+                                const targetElement = await page.$(selector);
+                                const actualInnerHTML = (await targetElement.innerHTML()).trim();
+                                const expectedInnerHTML = (await readFileContent(filename)).trim();
+                                expect(actualInnerHTML).toBe(expectedInnerHTML);
+                            });
+                            return chain;
+                        },
+                    }),
+                    browserURLEndsWith: (url: string) => {
+                        self.assertions.push(async (page: Page) => {
+                            expect(page.url().endsWith(url)).toBeTruthy();
                         });
                         return chain;
                     },
-                }),
-                browserURLEndsWith: (url: string) => {
-                    assertions.push(async (page: Page) => {
-                        expect(page.url().endsWith(url)).toBeTruthy();
-                    });
-                    return chain;
-                },
-                loadEvent: () => ({
-                    hasBeenDispatchedWithDetails: (expectedDetails: LoadEventDetail) => {
-                        const assertion = async (_: Page, eventLogger: EventLogger) => {
-                            const eventDetail = await new Promise<LoadEventDetail>(resolve => {
-                                eventLogger.subscribe({
-                                    notify: (eventDetail: LoadEventDetail) => {
-                                        resolve(eventDetail);
-                                    },
+                    loadEvent: () => ({
+                        hasBeenDispatchedWithDetails: (expectedDetails: LoadEventDetail) => {
+                            const assertion = async (_: Page, eventLogger: EventLogger) => {
+                                const eventDetail = await new Promise<LoadEventDetail>(resolve => {
+                                    eventLogger.subscribe({
+                                        notify: (eventDetail: LoadEventDetail) => {
+                                            resolve(eventDetail);
+                                        },
+                                    });
                                 });
-                            });
-                            expect(eventDetail).toEqual(expectedDetails);
-                        };
-                        assertions.push(assertion);
-                        return chain;
-                    },
-                }),
-            };
-        },
-    };
+                                expect(eventDetail).toEqual(expectedDetails);
+                            };
+                            self.assertions.push(assertion);
+                            return chain;
+                        },
+                    }),
+                };
+            },
+        };
 
-    const actionChain: WithPageContentFixtureActions = {
-        do: (callback: PageConsumer) => {
-            actions.push(callback);
-            return actionChain;
-        },
-        click: (selector: string) => {
-            actions.push(async (page: Page) => await page.click(selector));
-            return actionChain;
-        },
-        then: () => assertionChainStart,
-    };
+        const actionChain: WithPageContentFixtureActions = {
+            do: (callback: PageConsumer) => {
+                self.actions.push(callback);
+                return actionChain;
+            },
+            click: (selector: string) => {
+                self.actions.push(async (page: Page) => await page.click(selector));
+                return actionChain;
+            },
+            then: () => assertionChainStart,
+        };
 
-    return {
-        ...actionChain,
-        ...assertionChainStart,
-    };
+        return {
+            ...actionChain,
+            ...assertionChainStart,
+        };
+    }
 
-    async function runTest() {
+    async runTest() {
         let eventLogger: EventLogger;
 
-        const page = await prepareContext({
-            pageContent: html,
+        const page = await this.prepareContext({
+            pageContent: this.html,
             beforeLoadingLib: async (page: Page) => {
-                eventLogger = await createEventLogger(page);
+                eventLogger = await this.createEventLogger(page);
             },
         });
 
-        for (let action of actions)
+        for (let action of this.actions)
             await action(page);
 
-        await Promise.all(assertions.map(fn => fn(page, eventLogger)));
+        await Promise.all(this.assertions.map(fn => fn(page, eventLogger)));
     }
 }
